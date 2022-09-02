988c221377f67459b00ce5b3d46c8003775622f0
Chrisd313 <chrisd313@hotmail.com>
2022-09-02 11:36:41 +0100

    commit

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,33 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { SaucesList } from './SaucesList';
 
+import {Route, Routes} from "react-router-dom";
+
+
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
+import Navbar from './Navbar';
+import Home from './Home'
+import Create from './CreateForm';
 
 export const App = () => {
-
-	const [sauces, setSauces] = useState([]);
-
-	async function fetchSauces(){
-		try {
-			const response = await fetch(`${apiURL}/sauces`);
-			const saucesData = await response.json();
-			
-			setSauces(saucesData);
-		} catch (err) {
-			console.log("Oh no an error! ", err)
-		}
-	}
-
-	useEffect(() => {
-		fetchSauces();
-	}, []);
-
 	return (
-		<main>	
-      <h1>Sauce Store</h1>
-			<h2>All things 🔥</h2>
-			<SaucesList sauces={sauces} />
+		<>
+		<main>
+			<Navbar />
+			<Home />
 		</main>
+		</>
 	)
 }
---
+++ public/react/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Navbar() {
+    return <nav className="nav">
+        <a href="/" className="site-title">SITE NAME</a>
+        <ul>
+            <CustomLink href="/">Home</CustomLink>
+            <CustomLink href="/createitem">Create item</CustomLink>
+        </ul>
+    </nav>
+}
+
+function CustomLink({href, children, ...props}){
+    const path = window.location.pathname
+
+    return (
+        <li className={path === href ? "active" : ""}>
+            <a href={href}>{children}</a>
+        </li>
+    )
+}
